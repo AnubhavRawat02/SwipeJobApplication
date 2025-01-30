1b1f7027460b5c893ce1b0a4c4bfad74f5a26952
anubhavTechrange <anubhav@techrange.co>
2025-01-30 20:03:19 +0530

    Initial Commit

---
+++ SwipeJobApplication/SwipeJobApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeJobApplicationApp.swift
+//  SwipeJobApplication
+//
+//  Created by Anubhav Rawat on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SwipeJobApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
